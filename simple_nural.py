29c1772e46a1f0f684ebe13c3d97f390d84f0630
LeeHeonWoo1 <gjsdn245@naver.com>
2023-07-17 09:20:04 +0900

    시각화 부분 추가

--- simple_nural.py
+++ simple_nural.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy as np, matplotlib.pyplot as plt
 
 alpha = 1.0 # learning rate
 epoch = 5000 # number of learning iteration
@@ -17,12 +17,11 @@ def sigmoid(x): # 활성화 함수 정의
     return y
 
 input_data = np.array([-4, -3, -2, -1, 0, 1, 2, 3, 4, 5]) # 10개의 입력데이터
-teaching_data = []
-for i in input_data:
-    teaching_data.append(sigmoid(i)) # 각 입력값에 대한 sigmoid 계산값을 빈 리스트에 저장 = 타겟값 구성
+teaching_data = [sigmoid(i) for i in input_data]
 
-for n in range(1, epoch+1): # 5000회의 학습 진행
-    # n = 1
+result_array = [] # 각 학습에 대한 모든 결과를 담을 빈 리스트 생성
+for n in range(1, epoch + 1): # 5000회의 학습 진행
+    result_per_each = [] # 회당 학습 결과를 담을 리스트 생성 
     for i in range(len(input_data)): 
         x = input_data[i] # input값을 x에 할당
         t = teaching_data[i] # 타겟값은 t에 할당    
@@ -30,6 +29,7 @@ for n in range(1, epoch+1): # 5000회의 학습 진행
         u1 = sigmoid(w1*x + b1) # -5
         u2 = sigmoid(w2*x + b2) # 5
         y = sigmoid(w3*u1 + w4*u2 + b3)
+        result_per_each.append(y)
         # 역전파 계산
         E = 0.5*(y-t)**2 # 손실함수
         dE_dw_3 = (y-t)*(1-y)*y*u1 # 출력층 가중치
@@ -49,12 +49,19 @@ for n in range(1, epoch+1): # 5000회의 학습 진행
         b1 = b1 - alpha * dE_db_1
         b2 = b2 - alpha * dE_db_2
         b3 = b3 - alpha * dE_db_3
+    result_array.append(result_per_each)
         
     print(f"{n} Epoch - Error : {E}")
- 
+    
 x = 0.5
 u1 = sigmoid(w1*x + b1)
 u2 = sigmoid(w2*x + b2)
 y = sigmoid(w3*u1 + w4*u2 + b3)
-print("신경망의 예측값 {}".format(y)) 
-print("계산된 값(정답) : {}".format(sigmoid(x)))
+
+print(f"실측값 t : {sigmoid(x)}")
+print(f"신경망에 의해 예측된 값 y : {y}")
+
+plt.plot(input_data, teaching_data)
+for i in range(len(result_array)):
+    plt.plot(input_data, result_array[i])
+plt.show()
