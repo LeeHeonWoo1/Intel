586b85cc7edf0734e8561a0afa00a944239fc655
LeeHeonWoo1 <gjsdn245@naver.com>
2023-12-12 08:54:40 +0900

    게시글과 댓글 테이블 추가

--- GETI/web/geti/models.py
+++ GETI/web/geti/models.py
@@ -5,4 +5,18 @@ class User(db.Model):
     userid = db.Column(db.String(40), primary_key=True)
     userpw = db.Column(db.String(30), nullable=False)
     user_phone = db.Column(db.String(60), nullable=False)
-    
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text(), nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
+    user_id = db.Column(db.String(200), db.ForeignKey('user.userid', ondelete='CASCADE'), nullable=True, server_default='관리자')
+    user = db.relationship('User', backref=db.backref('question_set'))
+
+class Answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'))
+    question = db.relationship('Question', backref=db.backref('answer_set'))
+    content = db.Column(db.Text(), nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
